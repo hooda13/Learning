4da8e8dc4f3e82512e74d70f247ddc518739d0e3
hooda13 <hooda013@gmail.com>
2023-05-20 01:37:56 +0530

    dlskfm

--- cpp.cpp
+++ cpp.cpp
@@ -3,45 +3,15 @@
 
 using namespace std;
 
-class Employee {
-protected:
-    string name;
-    int age;
-    float salary;
-
+class Car {
 public:
-    Employee(string n, int a, float s) : name(n), age(a), salary(s) {}
-
-    virtual void display() {
-        cout << "Name: " << name << ", Age: " << age << ", Salary: " << salary << endl;
-    }
+    string color;
+    string make;
+    string model;
 };
 
-class Manager : public Employee {
-private:
-    int level;
-
-public:
-    Manager(string n, int a, float s, int l) : Employee(n, a, s), level(l) {}
-
-    void display() {
-        cout << "Name: " << name << ", Age: " << age << ", Salary: " << salary << ", Level: " << level << endl;
-    }
-};
 
 int main() {
-    Employee emp("John Doe", 30, 50000);
-    Manager mgr("Jane Smith", 35, 75000, 2);
-
-    emp.display();
-    mgr.display();
-
-    Employee *empPtr;
-    empPtr = &emp;
-    empPtr->display();
-
-    empPtr = &mgr;
-    empPtr->display();
-
+   
     return 5;
 }
